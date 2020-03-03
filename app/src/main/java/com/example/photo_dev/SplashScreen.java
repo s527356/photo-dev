09e4aaf99e73d272d43737e28dd9471749a5f03b
aawajjoshi <s521315@nwmissouri.edu>
2020-03-02 18:26:33 -0600

    Added a Splash Screen

---
+++ app/src/main/java/com/example/photo_dev/SplashScreen.java
@@ -0,0 +1,37 @@
+package com.example.photo_dev;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static final int SPLASH_DISPLAY_TIME = 2500;
+
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+
+                Intent intent = new Intent();
+                intent.setClass(SplashScreen.this,
+                        MainActivity.class);
+
+                SplashScreen.this.startActivity(intent);
+                SplashScreen.this.finish();
+
+            }
+        }, SPLASH_DISPLAY_TIME);
+
+
+    }
+}
