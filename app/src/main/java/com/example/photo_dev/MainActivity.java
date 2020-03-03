967844d691eff9ec8701b0f0d0aa9e215e36edbb
aawajjoshi <s521315@nwmissouri.edu>
2020-03-02 19:53:43 -0600

    Created a BubbleSeekBar in the Main Activity to pick the temperature.

--- app/src/main/java/com/example/photo_dev/MainActivity.java
+++ app/src/main/java/com/example/photo_dev/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.photo_dev;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextSwitcher;
+import android.widget.TextView;
+
+import com.xw.repo.BubbleSeekBar;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BubbleSeekBar bubbleSeekBar = (BubbleSeekBar)findViewById(R.id.seekBar);
+
+        final TextView textView = (TextView) findViewById(R.id.txtView);
+
+        bubbleSeekBar.setOnProgressChangedListener(new BubbleSeekBar.OnProgressChangedListener() {
+            @Override
+            public void onProgressChanged(BubbleSeekBar bubbleSeekBar, int progress, float progressFloat, boolean fromUser) {
+                textView.setText(String.format("Temperature: %d ˚Celsius", progress));
+            }
+
+            @Override
+            public void getProgressOnActionUp(BubbleSeekBar bubbleSeekBar, int progress, float progressFloat) {
+
+            }
+
+            @Override
+            public void getProgressOnFinally(BubbleSeekBar bubbleSeekBar, int progress, float progressFloat, boolean fromUser) {
+
+            }
+        });
     }
 }
