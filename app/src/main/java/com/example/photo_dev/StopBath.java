8154f74d488ed60029c4829fc99dbdff014c229d
Keenan Piveral-Brooks <me@keenanpb.dev>
2020-05-02 16:05:03 -0500

    Add stop bath timer

--- app/src/main/java/com/example/photo_dev/StopBath.java
+++ app/src/main/java/com/example/photo_dev/StopBath.java
@@ -1,16 +1,21 @@
 package com.example.photo_dev;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.airbnb.lottie.LottieAnimationView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class StopBath extends AppCompatActivity {
-
+    Button startBathBN, stopBathBN;
+    TextView bathTimerTV;
     LottieAnimationView lottieAnimationView;
     private static final int ANIMATION_DISPLAY_TIME = 3000;
 
@@ -19,26 +24,47 @@ public class StopBath extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_stop_bath);
 
+        startBathBN = findViewById(R.id.startBathBN);
+        stopBathBN = findViewById(R.id.stopBathBN);
+        bathTimerTV = findViewById(R.id.bathTimerTV);
+
+        startBathBN.setOnClickListener(v -> start());
+        stopBathBN.setOnClickListener(v -> stop());
     }
 
-    public void start(View v){
-        Button bathBTN = (Button) findViewById(R.id.bathBTN);
-        Button bathBTN2 = (Button) findViewById(R.id.bathBTN2);
+    public void start() {
+        new CountDownTimer(120000, 1000) {
+            @SuppressLint("SetTextI18n")
+            public void onTick(long millisUntilFinished) {
+                int second = (int) (millisUntilFinished / 1000);
+                bathTimerTV.setText("Agitate for " + second + " Seconds");
+            }
+
+            // Moves to stop bath on finish
+            public void onFinish() {
+                bathTimerTV.setText("");
+                Intent intent = new Intent();
+                intent.setClass(StopBath.this, Fixer.class);
+                StopBath.this.startActivity(intent);
+                StopBath.this.finish();
+            }
+        }.start();
 
         lottieAnimationView = findViewById(R.id.bathAnimation);
-
-        bathBTN.setVisibility(View.GONE);
-
+        startBathBN.setVisibility(View.GONE);
         lottieAnimationView.setVisibility(View.VISIBLE);
         lottieAnimationView.playAnimation();
+        stopBathBN.setVisibility(View.VISIBLE);
+    }
 
-        bathBTN2.setVisibility(View.VISIBLE);
+    public void stop() {
+
     }
 
     public void stop(View v) {
         lottieAnimationView = findViewById(R.id.bathAnimation);
         lottieAnimationView.setVisibility(View.GONE);
-        Button bathBTN2 = (Button) findViewById(R.id.bathBTN2);
+        Button bathBTN2 = (Button) findViewById(R.id.stopBathBN);
         bathBTN2.setVisibility(View.GONE);
     }
 }
