2c1effab95aa4eb896017424ba64b2035d98e9af
Keenan Piveral-Brooks <me@keenanpb.dev>
2020-05-02 13:35:21 -0500

    Add agitation stub

--- app/src/main/java/com/example/photo_dev/Developer.java
+++ app/src/main/java/com/example/photo_dev/Developer.java
@@ -1,5 +1,6 @@
 package com.example.photo_dev;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -14,17 +15,17 @@ import android.widget.TextView;
 
 public class Developer extends AppCompatActivity {
 
-    TextView timerTV;
+    TextView timerTV, agitateTimerTV;
     Button startDevBN, stopDevBN;
     LottieAnimationView lottieAnimationView;
     int temperature;
-    double totalTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_developer);
         timerTV = findViewById(R.id.timerTV);
+        agitateTimerTV = findViewById(R.id.agitateTimerTV);
         startDevBN = findViewById(R.id.startDevBN);
         stopDevBN = findViewById(R.id.stopDevBN);
         lottieAnimationView = findViewById(R.id.potionAnimation);
@@ -40,9 +41,11 @@ public class Developer extends AppCompatActivity {
         startDevBN.setVisibility(View.GONE);
 
         // Countdown timer for developer
-        new CountDownTimer(30000, 1000) {
+        new CountDownTimer(calculateTimer(), 1000) {
+            @SuppressLint("SetTextI18n")
             public void onTick(long millisUntilFinished) {
                 timerTV.setText(millisUntilFinished / 1000 + " Seconds");
+                // Agitate for 5 seconds every 30 seconds
             }
 
             // Moves to stop bath on finish
@@ -61,20 +64,37 @@ public class Developer extends AppCompatActivity {
     }
 
     // Finds total development time based on Kodak development chart for 400TX film
-    private void calculateTimer() {
+    private int calculateTimer() {
+        double totalTime;
         switch (temperature) {
-            case 65: totalTime = 7.5;
-            case 66: totalTime = 7.25;
-            case 67: totalTime = 7;
-            case 68: totalTime = 6.5;
-            case 69: totalTime = 6;
-            case 70: totalTime = 5.5;
-            case 71: totalTime = 5.25;
-            case 72: totalTime = 5;
-            case 73: totalTime = 4.75;
-            case 74: totalTime = 4.5;
-            case 75: totalTime = 4.25;
+            case 65: totalTime = 7.5; break;
+            case 66: totalTime = 7.25; break;
+            case 67: totalTime = 7; break;
+            case 68: totalTime = 6.5; break;
+            case 69: totalTime = 6; break;
+            case 71: totalTime = 5.25; break;
+            case 72: totalTime = 5; break;
+            case 73: totalTime = 4.75; break;
+            case 74: totalTime = 4.5; break;
+            case 75: totalTime = 4.25; break;
+            default: totalTime = 5.5; break; // also serves as 70 degrees
         }
+        return (int) (totalTime * 60000);
+    }
+
+    // Agitate for 5 seconds every 30 seconds
+    private void agitate() {
+        new CountDownTimer(5000, 1000) {
+            @SuppressLint("SetTextI18n")
+            public void onTick(long millisUntilFinished) {
+                timerTV.setText(millisUntilFinished / 1000 + " Seconds");
+            }
+
+            // Moves to stop bath on finish
+            public void onFinish() {
+                timerTV.setText("");
+            }
+        }.start();
     }
 
     // Should reset timer or go back to the beginning?
